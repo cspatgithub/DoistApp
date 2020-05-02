443a88c68b5d9a123f8780bfa331a80317320a1a
cspatgithub <cspat99@gmail.com>
2020-05-02 13:27:47 +0000

    still trying

--- root/settings.py
+++ root/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/css/'),
-    os.path.join(BASE_DIR, 'static/images/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
